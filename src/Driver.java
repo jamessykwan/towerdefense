f20bbd118c89825e3d7cc31362d1a1417cc9fb19
Yash Melkani <yashmelkani02@gmail.com>
2019-04-24 10:11:25 -0700

    Starter file

---
+++ src/Driver.java
@@ -0,0 +1,175 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.image.*;
+import java.awt.geom.AffineTransform;
+
+public class Driver extends JPanel implements ActionListener, KeyListener,
+		MouseListener, MouseMotionListener {
+
+	int screen_width = 900;
+	int screen_height = 900;
+	Sprite player;
+	Background bg;
+	int my_variable = 0; //example
+	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+	ArrayList<Particle> particles = new ArrayList<Particle>();
+	
+	//fonts
+	Font font = new Font("Courier New", 1, 50);
+	Font font2 = new Font("Courier New", 1, 30);
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		//bg.paint(g);
+		
+		g.setFont(font);
+		
+		g.setColor(Color.RED);
+		g.drawString(("my_variable:")+Integer.toString(my_variable), 0, 870);
+		g.setFont(font2);
+		g.setColor(Color.CYAN);
+	
+		//paint sprite
+		player.paint(g);
+		
+	
+		
+		g.setColor(Color.BLACK);
+		for(Particle p : particles){
+			p.paint(g);
+			
+		}
+		
+		
+		
+	
+	}
+
+
+	public void update() {
+	
+	  player.move();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		update();
+		repaint();
+	}
+
+	public static void main(String[] arg) {
+		Driver d = new Driver();
+	}
+
+	public Driver() {
+		JFrame f = new JFrame();
+		f.setTitle("Frogger");
+		f.setSize(screen_width, screen_height);
+		f.setResizable(false);
+		f.addKeyListener(this);
+		f.addMouseMotionListener(this);
+		
+		//sprite instantiation
+		player = new Sprite("frog.png");
+		player.addMouseListener(this);
+		
+		//particles
+		particles.add(new Particle(50,50));
+
+	
+		f.add(this);
+		
+		//drawing timer
+		t = new Timer(17, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+	Timer t;
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("key press "+e.getKeyCode());
+		if(e.getKeyCode()==38){
+			//up
+			player.vy=-1;
+		}
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+			
+		pressed = true;
+		if(e.getComponent().getClass()==Sprite.class){
+			System.out.println("clicked on a sprite");
+		}
+	
+	}
+	boolean pressed = false;
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public void reset() {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		pressed = false;
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		particles.add(new Particle(e.getX()-5,e.getY()-5));
+
+	}
+
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Particle.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Particle {
+	public int life = 50;
+	int x = 0, y = 0;
+	int vx = 0, vy = -90, ay = 1;
+	
+	public void paint(Graphics g){
+		g.setColor(Color.red);
+		g.fillOval(x, y, 10, 10);
+		x++;
+		
+	}
+	
+	public Particle(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+}
