6281423aa0ba72fd2f1e9c908727bf0a5383820a
DerrickhSun <derrick.han.sun@gmail.com>
2019-06-04 09:38:40 -0700

    test

--- src/game/TackShooter.java
+++ src/game/TackShooter.java
@@ -2,39 +2,44 @@ package game;
 
 import java.util.ArrayList;
 
+import game.GameEffects.Dart;
+
 public class TackShooter extends Tower{
 	public TackShooter(int x, int y) {
 		super(x, y, "../resources/tackShooter.png");
 		setAttackRadius(200);
 	}
-	public void findTarget(ArrayList<Balloon> balloons) {
-		if (getTarget() != null) {
-			return;
-
-		}
-		double closestDist = 0.0;
-		Sprite closestTarget = null;
-		for (Balloon t : balloons) {
-			System.out.println("detecting");
-
-			double distanceX = t.x+ 10 - this.getX();
-			double distanceY = t.y+10 - this.getY();
-
-			double distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2)) + 110;
-			System.out.println(distance);
-			if (Double.compare(distance, getAttackRadius()) > 0) {
-				continue;
-			}
-
-			if (closestTarget == null) {
-				closestDist = distance;
-				closestTarget = t;
-			} else if (Double.compare(distance, closestDist) < 0) {
-				closestDist = distance;
-				closestTarget = t;
-			}
-
-		}
-		setTarget((Balloon) closestTarget);
-	}
+	public void findTarget(ArrayList<Balloon> sprites) {
+        if (getTarget() != null) {
+            return;
+        }
+        double closestDist = 0.0;
+        Balloon closestTarget = null;
+        for (Balloon t : sprites) {
+            double distanceX = t.x + 10 - this.getX();
+            double distanceY = t.y + 10 - this.getY();
+            double distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2)) + 110;
+            //System.out.println(distance);
+            if (Double.compare(distance, getAttackRadius()) > 0) {
+                continue;
+            }
+            if (closestTarget == null) {
+                closestDist = distance;
+                closestTarget = t;
+            } else if (Double.compare(distance, closestDist) < 0) {
+                closestDist = distance;
+                closestTarget = t;
+            }
+        }
+        setTarget(closestTarget);
+        if (closestTarget != null) {
+            for(int i=0;i<8;i++) {
+            	Dart dart = new Dart(new Coordinate(getX(), getY()), new Coordinate((int) (getX()+getAttackRadius()*Math.cos(.785*i)), (int) (getY()+getAttackRadius()*Math.sin(.785*i))));
+            	Driver.gameEffects.add(dart);
+            	//closestTarget.takeDamage(1);
+            	setTarget(null);
+            }
+        }
+    }
+	
 }
